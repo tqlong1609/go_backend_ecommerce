1c3a4133450962e254a6158265775519e5e91b8b
long.tran <long.tran@kyanon.digital>
2025-01-03 17:57:48 +0700

    doing: setup mysql

--- global/global.go
+++ global/global.go
@@ -3,9 +3,11 @@ package global
 import (
 	"github.com/tqlong1609/go_backend_ecommerce/pkg/logger"
 	"github.com/tqlong1609/go_backend_ecommerce/pkg/setting"
+	"gorm.io/gorm"
 )
 
 var (
+	Mdb    *gorm.DB
 	Config *setting.Config
 	Logger *logger.LoggerZap
 )
--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -1,3 +1,33 @@
 package initialize
 
-func InitMySql() {}
+import (
+	"fmt"
+
+	"github.com/tqlong1609/go_backend_ecommerce/global"
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func checkErrorPanic(err error, msg string) {
+	if err != nil {
+		global.Logger.Error(msg, zap.Error(err))
+		panic(err)
+	}
+}
+
+func InitMySql() {
+	m := global.Config.Mysql
+	dsn := "%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	var s = fmt.Sprintf(dsn, m.Username, m.Password, m.Host, m.Port, m.Database)
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		SkipDefaultTransaction: false,
+	})
+
+	checkErrorPanic(err, "mysql init error")
+	global.Logger.Info("mysql init success")
+	global.Mdb = db
+
+	// Set Pool
+
+}
